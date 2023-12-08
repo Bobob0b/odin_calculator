3c05e71b8ace25c40560aa72811963e4d25b70cf
Bobob0b <github@mzx.at>
2023-12-08 02:37:48 +0100

    calculation works but has few bugs. needs to be reworked

--- js.js
+++ js.js
@@ -1,8 +1,60 @@
+let newNumber = 0;
+let currentSum = 0;
+let operator = "+";
+let displayValue = ""; // start with a new number
 
-let num1;
-let num2;
-let operator;
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const arrButtons = Array.from(buttons);
 
+arrButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        evaluateInput(button.value);
+    });
+});
+
+
+
+
+// gets a value and writes it onto the display
+function evaluateInput(val) {
+
+    if (!isNaN(val.valueOf())) { // integers
+        displayValue = `${displayValue}${val}`
+        newNumber = parseInt(displayValue)
+    } else {
+
+        // anything else
+        if (val == "=") {
+            newNumber = parseInt(displayValue);
+            currentSum = operate(currentSum, newNumber, operator);
+            displayValue = `${currentSum}`;
+        }
+        if (val == "+" || val == "-" || val == "*" || val == "/") {
+            currentSum = parseInt(operate(currentSum, newNumber, operator));
+            operator = val;
+            newNumber = 0;
+            displayValue = `${newNumber}`;
+        }
+        if (val == "clear") {
+            newNumber = 0;
+            currentSum = 0;
+            operator = "+";
+            displayValue = "0";
+        }
+    }
+
+    display.textContent = displayValue;
+
+}
+
+
+
+// gets a value and writes it onto the display
+function updateDisplay(val) {
+
+
+}
 
 // evaluates the operator and calls the appropriate calculation function
 function operate(a, b, op) {
